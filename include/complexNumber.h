433d11b9c69188726f9ff7069b48bb42eac2a54b
VadimDenisovich <vadimDenis0vich@yandex.com>
2024-12-12 15:09:02 +1000

    Complex number v3

--- include/complexNumber.h
+++ include/complexNumber.h
@@ -7,31 +7,58 @@
 #include <type_traits>
 #include <stdexcept>
 
-template<typename T>
-class ComplexNumber {
-    // Вызывает ошибку, если тип T не соответствует int, float, double 
-    // EX: СomplexNumber<char> вызовет ошибку
-    static_assert(std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, double>,
-                  "Unsupported type for ComplexNumber");
+// Класс действительного числа
+class Real {
+private:
+	double value; 
 
+public: 
+	// Конструктор для действительного числа
+	template<typename T>
+	Real(T _real) {
+		static_assert(std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, double>,
+							"Unsupported type for real part");
+		value = static_cast<double>(_real);
+	};
+
+	friend class ComplexNumber; 
+};
+
+// Класс мнимой части
+class Imag {
+private: 
+	double value; 
+
+public: 
+	// Конструктор мнимой части 
+	template<typename T>
+	Imag(T _imag) {
+		static_assert(std::is_same_v<T, int> || std::is_same_v<T, float> || std::is_same_v<T, double>,
+							"Unsupported type for imaginary part");
+		value = static_cast<double>(_imag);
+	};
+
+	friend class ComplexNumber; 
+};
+
+class ComplexNumber {
     // Указывать в .h файле параметры по умолчанию 
     // factorization(int degree = 1)
 private: 
-    double real;
-    double imaginary; 
+    Real real;
+    Imag imaginary; 
 
 protected:
     template<typename V>
     static double toDouble(const V& value);
 
 public:   
-    ComplexNumber(T r = 0.0, double i = 0.0); 
-    double getReal();
-
+    ComplexNumber(Real r = Real(0.0), Imag i = Imag(0.0)); 
+    double getReal() const;
     // Чтобы работало auto нужно в tasks.json 
     // в разделе args прописать `-std=c++20`
     void setReal(auto _real); 
-    double getImaginary(); 
+    double getImaginary() const; 
     void setImaginary(auto _imaginary);
     double abs();
     double arg();
@@ -49,57 +76,45 @@ public:
     ComplexNumber operator-();
 
     // Дружественные функции 
-    template<typename U>
-    friend std::ostream &operator<<(std::ostream &terminal, const ComplexNumber<U> &number); 
-    
-    template<typename U>
-    friend ComplexNumber<double> operator+(ComplexNumber<U> &left, ComplexNumber<U> &right);
+    friend std::ostream &operator<<(std::ostream &terminal, const ComplexNumber &number); 
+    friend ComplexNumber operator+(ComplexNumber &left, ComplexNumber &right);
     
     // Указываем два шаблона 
     // typename U - для типа, с которым был инициализирован экземпляр класса 
     // typename V - для типа, с каким 
-    template<typename U, typename V>
-    friend ComplexNumber<double> operator+(ComplexNumber<U> &left, V number);
+    template<typename V>
+    friend ComplexNumber operator+(ComplexNumber &left, V number);
     
-    template<typename U, typename V>
-    friend ComplexNumber<double> operator+(V number, ComplexNumber<U> &right);
+    template<typename V>
+    friend ComplexNumber operator+(V number, ComplexNumber &right);
     
-    template<typename U>
-    friend ComplexNumber<double> operator-(ComplexNumber<U> &left, ComplexNumber<U> &right);
+    friend ComplexNumber operator-(ComplexNumber &left, ComplexNumber &right);
     
-    template<typename U, typename V>
-    friend ComplexNumber<double> operator-(ComplexNumber<U> &left, V number);
+    template<typename V>
+    friend ComplexNumber operator-(ComplexNumber &left, V number);
     
-    template<typename U, typename V>
-    friend ComplexNumber<double> operator-(V number, ComplexNumber<U> &right);
+    template<typename V>
+    friend ComplexNumber operator-(V number, ComplexNumber &right);
+  
+    friend ComplexNumber operator*(ComplexNumber &left, ComplexNumber &right);
     
-    template<typename U>
-    friend ComplexNumber<double> operator*(ComplexNumber<U> &left, ComplexNumber<U> &right);
+    template<typename V>
+    friend ComplexNumber operator*(ComplexNumber &left, V number);
     
-    template<typename U, typename V>
-    friend ComplexNumber<double> operator*(ComplexNumber<U> &left, V number);
+    template<typename V>
+    friend ComplexNumber operator*(V number, ComplexNumber &right);
     
-    template<typename U, typename V>
-    friend ComplexNumber<double> operator*(V number, ComplexNumber<U> &right);
+    friend ComplexNumber operator/(ComplexNumber &left, ComplexNumber &right);
     
-    template<typename U>
-    friend ComplexNumber<double> operator/(ComplexNumber<U> &left, ComplexNumber<U> &right);
-    
-    template<typename U, typename V>
-    friend ComplexNumber<double> operator/(ComplexNumber<U> &left, V number);
+    template<typename V>
+    friend ComplexNumber operator/(ComplexNumber &left, V number);
 
-    template<typename U, typename V>
-    friend ComplexNumber<double> operator/(V number, ComplexNumber<U> &right);
+    template<typename V>
+    friend ComplexNumber operator/(V number, ComplexNumber &right);
 
-    template<typename U>
-    friend bool operator==(ComplexNumber<U> &left, ComplexNumber<U> &right);
+    friend bool operator==(ComplexNumber &left, ComplexNumber &right);
 
-    template<typename U>
-    friend bool operator!=(ComplexNumber<U> &left, ComplexNumber<U> &right);
+    friend bool operator!=(ComplexNumber &left, ComplexNumber &right);
 };
-
-// в конце нужно экспортировать файл с реализацией
-// шаблонных методов и конструкторов, и дружественных функций 
-#include "../src/complexNumber.tpp"
 
 #endif
--- tests/test_complexNumber.cpp
+++ tests/test_complexNumber.cpp
@@ -9,25 +9,25 @@
 TEST_CASE("Checking the class constructor for implicit conversion", "[constructor]"){
     // Преобразование из отрицательных и положительных int
     SECTION("Conversion from negative and positive int") {
-        ComplexNumber<int> c1(-1);
+        ComplexNumber c1(Real(-1));
         REQUIRE(c1.getReal() == Catch::Approx(-1.0));
-        ComplexNumber<int> c2(-1011);
+        ComplexNumber c2(Real(-1011));
         REQUIRE(c2.getReal() == Catch::Approx(-1011.0)); 
-        ComplexNumber<int> c3(50);
+        ComplexNumber c3(Real(50));
         REQUIRE(c3.getReal() == Catch::Approx(50.0));
-        ComplexNumber<int> c4(9999999);
+        ComplexNumber c4(Real(9999999));
         REQUIRE(c4.getReal() == Catch::Approx(9999999.0));
     }
     
     // Преобразование из положительных и отрицательных float 
     SECTION("Conversion from negative and positive float") {
-        ComplexNumber<float> c1(-1.0);
+        ComplexNumber c1(Real(-1.0));
         REQUIRE(c1.getReal() == Catch::Approx(-1.0));
-        ComplexNumber<float> c2(-1010.0);
+        ComplexNumber c2(Real(-1010.0));
         REQUIRE(c2.getReal() == Catch::Approx(-1010.0));
-        ComplexNumber<float> c3(5.0);
+        ComplexNumber c3(Real(5.0));
         REQUIRE(c3.getReal() == Catch::Approx(5.0));
-        ComplexNumber<float> c4(99999.0);
+        ComplexNumber c4(Real(99999.0));
         REQUIRE(c4.getReal() == Catch::Approx(99999.0));
     }
 };
@@ -35,18 +35,18 @@ TEST_CASE("Checking the class constructor for implicit conversion", "[constructo
 // Проверяем методы abs(), arg(), factorization()
 TEST_CASE("Testing methods abs(), arg(), factorization()", "[methods]") {
     SECTION("abs() method") {
-        ComplexNumber<int> c1(3, 4);
+        ComplexNumber c1(Real(3), Imag(4));
         REQUIRE(c1.abs() == Catch::Approx(5.0));
 
-        ComplexNumber<float> c2(3.0, 4.0);
+        ComplexNumber c2(Real(3.0), Imag(4.0));
         REQUIRE(c2.abs() == Catch::Approx(5.0));
     }
 
     SECTION("arg() method") {
-        ComplexNumber<int> c1(1, 1);
+        ComplexNumber c1(Real(1), Imag(1));
         REQUIRE(c1.arg() == Catch::Approx(0.785398).epsilon(0.01)); // pi/4
 
-        ComplexNumber<float> c2(1.0, 1.0);
+        ComplexNumber c2(Real(1.0), Imag(1.0));
         REQUIRE(c2.arg() == Catch::Approx(0.785398).epsilon(0.01)); 
     }
 
@@ -65,12 +65,12 @@ TEST_CASE("Testing methods abs(), arg(), factorization()", "[methods]") {
     // то абсолютная погрешность равна: |98 - 100| = 2
 
     SECTION("factorization() method") {
-        ComplexNumber<double> c1(4.0, 4.0);
+        ComplexNumber c1(Real(4.0), Imag(4.0));
         c1.factorization(2);
         REQUIRE(c1.getReal() == Catch::Approx(0.00000000000000196).epsilon(0.01));
         REQUIRE(c1.getImaginary() == Catch::Approx(32.0).epsilon(0.01));
 
-        ComplexNumber<double> c2(-10.0, 19.0);
+        ComplexNumber c2(Real(-10.0), Imag(19.0));
         c2.factorization(2);
         REQUIRE(c2.getReal() == Catch::Approx(-261));
         REQUIRE(c2.getImaginary() == Catch::Approx(-380));
@@ -80,69 +80,69 @@ TEST_CASE("Testing methods abs(), arg(), factorization()", "[methods]") {
 // Проверяем перегруженные операторы +, -, *, / и унарного минуса
 TEST_CASE("Testing overloaded operators +, -, *, / and unary minus", "[operators]") {
     SECTION("Addition operator") {
-        ComplexNumber<int> c1(1, 2);
-        ComplexNumber<int> c2(3, 4);
-        ComplexNumber<double> result = c1 + c2;
+        ComplexNumber c1(Real(1), Imag(2));
+        ComplexNumber c2(Real(3), Imag(4));
+        ComplexNumber result = c1 + c2;
         REQUIRE(result.getReal() == Catch::Approx(4.0));
         REQUIRE(result.getImaginary() == Catch::Approx(6.0));
 
-        ComplexNumber<float> c3(1.5, 2.5);
-        ComplexNumber<float> c4(3.5, 4.5);
-        ComplexNumber<double> result2 = c3 + c4;
+        ComplexNumber c3(Real(1.5), Imag(2.5));
+        ComplexNumber c4(Real(3.5), Imag(4.5));
+        ComplexNumber result2 = c3 + c4;
         REQUIRE(result2.getReal() == Catch::Approx(5.0));
         REQUIRE(result2.getImaginary() == Catch::Approx(7.0));
     }
 
     SECTION("Subtraction operator") {
-        ComplexNumber<int> c1(5, 6);
-        ComplexNumber<int> c2(3, 4);
-        ComplexNumber<double> result = c1 - c2;
+        ComplexNumber c1(Real(5), Imag(6));
+        ComplexNumber c2(Real(3), Imag(4));
+        ComplexNumber result = c1 - c2;
         REQUIRE(result.getReal() == Catch::Approx(2.0));
         REQUIRE(result.getImaginary() == Catch::Approx(2.0));
 
-        ComplexNumber<float> c3(5.5, 6.5);
-        ComplexNumber<float> c4(3.5, 4.5);
-        ComplexNumber<double> result2 = c3 - c4;
+        ComplexNumber c3(Real(5.5), Imag(6.5));
+        ComplexNumber c4(Real(3.5), Imag(4.5));
+        ComplexNumber result2 = c3 - c4;
         REQUIRE(result2.getReal() == Catch::Approx(2.0));
         REQUIRE(result2.getImaginary() == Catch::Approx(2.0));
     }
 
     SECTION("Multiplication operator") {
-        ComplexNumber<int> c1(1, 2);
-        ComplexNumber<int> c2(3, 4);
-        ComplexNumber<double> result = c1 * c2;
+        ComplexNumber c1(Real(1), Imag(2));
+        ComplexNumber c2(Real(3), Imag(4));
+        ComplexNumber result = c1 * c2;
         REQUIRE(result.getReal() == Catch::Approx(-5.0));
         REQUIRE(result.getImaginary() == Catch::Approx(10.0));
 
-        ComplexNumber<float> c3(1.5, 2.5);
-        ComplexNumber<float> c4(3.5, 4.5);
-        ComplexNumber<double> result2 = c3 * c4;
+        ComplexNumber c3(Real(1.5), Imag(2.5));
+        ComplexNumber c4(Real(3.5), Imag(4.5));
+        ComplexNumber result2 = c3 * c4;
         REQUIRE(result2.getReal() == Catch::Approx(-6));
         REQUIRE(result2.getImaginary() == Catch::Approx(15.5));
     }
 
     SECTION("Division operator") {
-        ComplexNumber<int> c1(1, 2);
-        ComplexNumber<int> c2(3, 4);
-        ComplexNumber<double> result = c1 / c2;
+        ComplexNumber c1(Real(1), Imag(2));
+        ComplexNumber c2(Real(3), Imag(4));
+        ComplexNumber result = c1 / c2;
         REQUIRE(result.getReal() == Catch::Approx(0.44).epsilon(0.01));
         REQUIRE(result.getImaginary() == Catch::Approx(0.08).epsilon(0.01));
 
-        ComplexNumber<float> c3(1.5, 2.5);
-        ComplexNumber<float> c4(3.5, 4.5);
-        ComplexNumber<double> result2 = c3 / c4;
+        ComplexNumber c3(Real(1.5), Imag(2.5));
+        ComplexNumber c4(Real(3.5), Imag(4.5));
+        ComplexNumber result2 = c3 / c4;
         REQUIRE(result2.getReal() == Catch::Approx(0.507).margin(0.0007));
         REQUIRE(result2.getImaginary() == Catch::Approx(0.06).margin(0.002));
     }
 
     SECTION("Unary minus operator") {
-        ComplexNumber<int> c1(1, -2);
-        ComplexNumber<int> result = -c1;
+        ComplexNumber c1(Real(1), Imag(-2));
+        ComplexNumber result = -c1;
         REQUIRE(result.getReal() == Catch::Approx(-1.0));
         REQUIRE(result.getImaginary() == Catch::Approx(2.0));
 
-        ComplexNumber<float> c2(1.5, -2.5);
-        ComplexNumber<float> result2 = -c2;
+        ComplexNumber c2(Real(1.5), Imag(-2.5));
+        ComplexNumber result2 = -c2;
         REQUIRE(result2.getReal() == Catch::Approx(-1.5));
         REQUIRE(result2.getImaginary() == Catch::Approx(2.5));
     }
@@ -151,32 +151,32 @@ TEST_CASE("Testing overloaded operators +, -, *, / and unary minus", "[operators
 // Тестируем операторы +=, -=, *=, /=
 TEST_CASE("Testing operators +=, -=, *=, /=", "[operators]") {
     SECTION("Operator +=") {
-        ComplexNumber<double> c1(1.0, 2.0);
-        ComplexNumber<double> c2(3.0, 4.0);
+        ComplexNumber c1(Real(1.0), Imag(2.0));
+        ComplexNumber c2(Real(3.0), Imag(4.0));
         c1 += c2;
         REQUIRE(c1.getReal() == Catch::Approx(4.0));
         REQUIRE(c1.getImaginary() == Catch::Approx(6.0));
     }
 
     SECTION("Operator -=") {
-        ComplexNumber<double> c1(5.0, 6.0);
-        ComplexNumber<double> c2(1.0, 2.0);
+        ComplexNumber c1(Real(5.0), Imag(6.0));
+        ComplexNumber c2(Real(1.0), Imag(2.0));
         c1 -= c2;
         REQUIRE(c1.getReal() == Catch::Approx(4.0));
         REQUIRE(c1.getImaginary() == Catch::Approx(4.0));
     }
 
     SECTION("Operator *=") {
-        ComplexNumber<double> c1(1.0, 2.0);
-        ComplexNumber<double> c2(3.0, 4.0);
+        ComplexNumber c1(Real(1.0), Imag(2.0));
+        ComplexNumber c2(Real(3.0), Imag(4.0));
         c1 *= c2;
         REQUIRE(c1.getReal() == Catch::Approx(-5.0));
         REQUIRE(c1.getImaginary() == Catch::Approx(10.0));
     }
 
     SECTION("Operator /=") {
-        ComplexNumber<double> c1(1.0, 2.0);
-        ComplexNumber<double> c2(3.0, 4.0);
+        ComplexNumber c1(Real(1.0), Imag(2.0));
+        ComplexNumber c2(Real(3.0), Imag(4.0));
         c1 /= c2;
         REQUIRE(c1.getReal() == Catch::Approx(0.44).epsilon(0.01));
         REQUIRE(c1.getImaginary() == Catch::Approx(0.08).epsilon(0.01));
@@ -186,22 +186,22 @@ TEST_CASE("Testing operators +=, -=, *=, /=", "[operators]") {
 // Тестируем операторы == и !=
 TEST_CASE("Testing comparison operators == and !=", "[operators]") {
     SECTION("Operator ==") {
-        ComplexNumber<double> c1(1.0, 2.0);
-        ComplexNumber<double> c2(1.0, 2.0);
+        ComplexNumber c1(Real(1.0), Imag(2.0));
+        ComplexNumber c2(Real(1.0), Imag(2.0));
         REQUIRE(c1 == c2);
     }
 
     SECTION("Operator !=") {
-        ComplexNumber<double> c1(1.0, 2.0);
-        ComplexNumber<double> c2(2.0, 3.0);
+        ComplexNumber c1(Real(1.0), Imag(2.0));
+        ComplexNumber c2(Real(2.0), Imag(3.0));
         REQUIRE(c1 != c2);
     }
 }
 
 // Проверяем, как работает оператор присваивания
 TEST_CASE("Testing assignment operator", "[assignment]") {
-    ComplexNumber<double> c1(1.0, 2.0);
-    ComplexNumber<double> c2;
+    ComplexNumber c1(Real(1.0), Imag(2.0));
+    ComplexNumber c2;
     c2 = c1;
     REQUIRE(c2.getReal() == Catch::Approx(1.0));
     REQUIRE(c2.getImaginary() == Catch::Approx(2.0));
